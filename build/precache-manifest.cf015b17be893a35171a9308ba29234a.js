646f4279f04b22092a33fe381d3688ec5747e5eb
Hammad <106879502+Hammad-Khurshid@users.noreply.github.com>
2023-08-02 11:51:54 +0000

    upload

---
+++ build/precache-manifest.cf015b17be893a35171a9308ba29234a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f03aa4844e379cc651c6",
+    "url": "/static/css/main.3cd4eecb.chunk.css"
+  },
+  {
+    "revision": "f03aa4844e379cc651c6",
+    "url": "/static/js/main.f03aa484.chunk.js"
+  },
+  {
+    "revision": "8661c4af1aa50a4cec73",
+    "url": "/static/js/1.8661c4af.chunk.js"
+  },
+  {
+    "revision": "c6d2d23f817d2695d263",
+    "url": "/static/css/2.98f59603.chunk.css"
+  },
+  {
+    "revision": "c6d2d23f817d2695d263",
+    "url": "/static/js/2.c6d2d23f.chunk.js"
+  },
+  {
+    "revision": "c5827c058dc15ddfd63a",
+    "url": "/static/js/runtime~main.c5827c05.js"
+  },
+  {
+    "revision": "51f28e5b8f39324212a1c461784cc1c8",
+    "url": "/static/media/brain1.51f28e5b.png"
+  },
+  {
+    "revision": "e281749002f3ec35df7a0563701634c3",
+    "url": "/index.html"
+  }
+];
